4535ebdc6be07cf5c648b8fa7651fe6a5f29ccc8
Ogdol, Kim Adrie <andriepekpek@gmail.com>
2026-05-09 02:42:02 +0800

    Fix: Update Fallback, update routing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getToken, logoutUser } from './auth'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://tuloclicks-it-elect-2-production.up.railway.app/api',
 })
 
 api.interceptors.request.use(
